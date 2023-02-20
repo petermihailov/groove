041ecc24939742a70200106e39f73a0190a05af5
pmihailov <usernameispeter@gmail.com>
2023-02-20 23:54:15 +0400

    chore: some changes

--- src/components/Editor/Bar/Bar.tsx
+++ src/components/Editor/Bar/Bar.tsx
@@ -35,39 +35,11 @@ const Bar = ({
   tracking,
   onClick,
 }: BarProps) => {
-  // const [actions, setActions] = useState(false);
-  // const actionsRef = useRef(null);
-
   const refTracker = useRef<SVGRectElement>(null);
 
   const instrumentsByGroups = useMemo(() => {
     return convertBarInstrumentsByGroups(bar, enabledGroups);
   }, [bar, enabledGroups]);
-
-  // const hhFootOffsetGroups = useMemo(() => {
-  //   return Object.values(enabledGroups).filter(Boolean).length - Number(enabledGroups.cy);
-  // }, [enabledGroups]);
-
-  // const toggleActions = () => setActions((prev) => !prev);
-
-  // const closeActions = () => setActions(false);
-
-  // const addBar = () => {
-  //   onAddBar(barIndex);
-  //   closeActions();
-  // };
-
-  // const removeBar = () => {
-  //   onRemoveBar(barIndex);
-  //   closeActions();
-  // };
-
-  // const clearBar = () => {
-  //   onClearBar(barIndex);
-  //   closeActions();
-  // };
-
-  // useClickOutside(actionsRef, closeActions);
 
   const colsCount = bar.length;
   const rowsCount = Object.keys(instrumentsByGroups).length;
@@ -156,56 +128,6 @@ const Bar = ({
       })}
     </svg>
   );
-
-  // return (
-  //   <div
-  //     className={clsx(className, classes.root)}
-  //     data-index={barIndex}
-  //     style={{ gridTemplateColumns: `repeat(${bar.length}, auto)` }}
-  //     onClick={onClick}
-  //   >
-  //     {enabledGroups.cy && <BarLine group="cy" line={instrumentsByGroups.cy} />}
-  //     {enabledGroups.hh && (
-  //       <BarLine group="hh" hhFootOffsetGroups={hhFootOffsetGroups} line={instrumentsByGroups.hh} />
-  //     )}
-  //     {enabledGroups.t1 && <BarLine group="t1" line={instrumentsByGroups.t1} />}
-  //     {enabledGroups.sn && <BarLine group="sn" line={instrumentsByGroups.sn} />}
-  //     {enabledGroups.t2 && <BarLine group="t2" line={instrumentsByGroups.t2} />}
-  //     {enabledGroups.t3 && <BarLine group="t3" line={instrumentsByGroups.t3} />}
-  //     {enabledGroups.ki && <BarLine group="ki" line={instrumentsByGroups.ki} />}
-  //
-  //     {/*<div ref={actionsRef} className={classes.actions}>*/}
-  //     {/*  <ButtonIcon*/}
-  //     {/*    aria-label={`${actions ? 'open' : 'close'} bar actions`}*/}
-  //     {/*    className={classes.actionButton}*/}
-  //     {/*    onClick={toggleActions}*/}
-  //     {/*  >*/}
-  //     {/*    <Icon name="icon.more" />*/}
-  //     {/*  </ButtonIcon>*/}
-  //
-  //     {/*  <Pill className={clsx(classes.actionBar, { [classes.actionsBarHidden]: !actions })}>*/}
-  //     {/*    <ButtonIcon aria-label="remove bar" onClick={removeBar}>*/}
-  //     {/*      <Icon name="icon.delete" />*/}
-  //     {/*    </ButtonIcon>*/}
-  //     {/*    <ButtonIcon aria-label="clear bar" onClick={clearBar}>*/}
-  //     {/*      <Icon name="icon.clear" />*/}
-  //     {/*    </ButtonIcon>*/}
-  //     {/*    <ButtonIcon aria-label="add bar" onClick={addBar}>*/}
-  //     {/*      <Icon name="icon.add" />*/}
-  //     {/*    </ButtonIcon>*/}
-  //
-  //     {/*    <TimeSignature*/}
-  //     {/*      barIndex={barIndex}*/}
-  //     {/*      beatsPerBar={bar.beatsPerBar}*/}
-  //     {/*      className={classes.timeSignature}*/}
-  //     {/*      noteValue={bar.noteValue}*/}
-  //     {/*      timeDivision={bar.timeDivision}*/}
-  //     {/*      onChangeSignature={onChangeSignature}*/}
-  //     {/*    />*/}
-  //     {/*  </Pill>*/}
-  //     {/*</div>*/}
-  //   </div>
-  // );
 };
 
 export default memo(Bar);
--- src/components/Editor/TimeSignature/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TimeSignature } from './TimeSignature';
-export type { TimeSignatureProps } from './TimeSignature';
